51156e4ab998cbd4ac1efbd6fefe5015715ac919
Yinka <asyinka@gmail.com>
2023-05-18 18:19:37 +0100

    chore: add button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Button from "./components/button";
 import "./index.css";
 
 function App() {
@@ -7,7 +8,24 @@ function App() {
       <div className="app-title-container">
         <h1 className="app-title">PERSONAL TASK MANAGER</h1>
       </div>
-      {/* <div className="tasks-panel"></div> */}
+
+      <div className="tasks-panel">
+        <div className="left-panel left-panel--light">
+          <div className="left-panel--heading">
+            <hr className="line" /> Categories <hr className="line" />
+          </div>
+          <Button buttonType={"button button--primary"}>Completed </Button>
+          <Button buttonType={"button button--accent"}>
+          <Button buttonType={"button button--secondary"}>
+          <Button buttonType={"button button--tertiary"}>
+          <Button buttonType={"button button--lb"}>
+          <Button buttonType={"button button--lb"}>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
