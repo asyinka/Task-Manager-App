29fca3e3ff1e8f0b6bdb648f8c1c0c23c94a9c81
Yinka <asyinka@gmail.com>
2023-05-18 14:43:30 +0100

    chore: add application title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="app-title-container">
+        <h1 className="app-title">PERSONAL TASK MANAGER</h1>
+      </div>
+      {/* <div className="tasks-panel"></div> */}
+    </div>
+  );
 }
 
 export default App;
