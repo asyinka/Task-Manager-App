c3c767b2a7e63295f8366ec82a57425a18a23897
Yinka <asyinka@gmail.com>
2023-05-19 02:57:50 +0100

    chore: add right panel header

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       </div>
 
       <div className="tasks-panel">
-        <div className="left-panel left-panel--light">
+        <div className="left-panel panel--light">
           <div className="left-panel--heading">
             <hr className="line" /> Categories <hr className="line" />
           </div>
@@ -19,11 +19,20 @@ function App() {
           <Button buttonType={"button button--secondary"}> Important </Button>
           <Button buttonType={"button button--tertiary"}> Later </Button>
           <Button buttonType={"button button--lb"}> To study </Button>
-          <Button buttonType={"button add-button"}>Add category</Button>
+          <Button buttonType={"button add-catg-button button-dark"}>
+            Add category
+          </Button>
         </div>
-        <div>
-          <div></div>
-          <div></div>
+
+        <div className="right-panel">
+          <div className="right-panel-header panel--light">
+            <span className="list-number header__item">5 tasks</span>
+            <Button buttonType={"button button-dark"}> Add new task </Button>
+            <span className="header__item">Clear Completed</span>
+          </div>
+          <div className="right-panel-lists panel--light">
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
